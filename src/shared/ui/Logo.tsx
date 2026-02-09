212d49b44942215c53ad2ff1bd2906b4e92b4bc3
mmeat512 <58766047+mmeat512@users.noreply.github.com>
2026-02-10 01:02:37 +0900

    fix: 로고 수정

--- src/shared/ui/Logo.tsx
+++ src/shared/ui/Logo.tsx
@@ -1,5 +1,6 @@
 'use client';
-import LogoIcon from '@/assets/icons/logo.svg';
+import LogoIcon from '@/assets/icons/LOGO.png';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 export default function Logo() {
@@ -10,8 +11,8 @@ export default function Logo() {
 
   return (
     <div onClick={handleClick} className="cursor-pointer flex justify-center items-center gap-3">
-      <LogoIcon />
-      <h1 className="text-gray-900 text-[26.5px] font-bold leading-normal">SIMVEX</h1>
+      <Image src={LogoIcon} alt="Logo" width={100} height={100} />
+      {/* <h1 className="text-gray-900 text-[26.5px] font-bold leading-normal">SIMVEX</h1> */}
     </div>
   );
 }
