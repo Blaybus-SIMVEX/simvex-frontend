157ff85f19684b211a45e80a57db0c818c1c0b54
sunny-son <thstkd92@gmail.com>
2026-02-10 15:58:19 +0900

    style : 페이지 레이아웃 수정

--- src/app/(main)/study/[detail]/page.tsx
+++ src/app/(main)/study/[detail]/page.tsx
@@ -30,7 +30,7 @@ export default async function StudyDetail({ params }: StudyDetailProps) {
               <AIAssistant objectId={objectId} />
             </div>
 
-            <div className="h-full flex-1 min-h-0">
+            <div className="shrink-0">
               <MemoPad objectId={objectId} />
             </div>
           </div>
